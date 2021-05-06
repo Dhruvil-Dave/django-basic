2c7693db7dd7a4b9f2cb354fae625c17b9be462d
Dhruvil Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2021-05-06 15:30:49 +0530

    Update views.py

--- testutils/views.py
+++ testutils/views.py
@@ -1,5 +1,5 @@
 
-# I have created this file - Harry
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -9,17 +9,17 @@ def index(request):
 
 
 def analyze(request):
-    #Get the text
+    
     djtext = request.POST.get('text', 'default')
 
-    # Check checkbox values
+    
     removepunc = request.POST.get('removepunc', 'off')
     fullcaps = request.POST.get('fullcaps', 'off')
     newlineremover = request.POST.get('newlineremover', 'off')
     extraspaceremover = request.POST.get('extraspaceremover', 'off')
     numberremover = request.POST.get('numberremover', 'off')
 
-    #Check which checkbox is on
+    
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
         analyzed = ""
